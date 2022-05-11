fbec805521048c9eff5b705df29485f2b6fa042f
Matt Burns <pidiegames@gmail.com>
2022-05-11 18:05:16 -0400

    built on Currency based on GameFlag class

--- Assets/Scripts/Inventory/Currency/Currency.cs
+++ Assets/Scripts/Inventory/Currency/Currency.cs
@@ -8,8 +8,12 @@ namespace Inventory.Currency
 
         public int Quantity => quantity;
 
+        public CurrencyData currencyData { get; private set; }
+
         public Currency() => quantity = 0;
         public Currency(int value) => quantity = value;
+
+        public Bind(CurrencyData data) => currencyData = data;
     }
 
     public abstract class Currency<T> : Currency where T : IAccruable
@@ -18,5 +22,11 @@ namespace Inventory.Currency
         public override Currency<T> operator -(Currency<T> a, Currency<T> b) => new Currency<T>(a.Quantity - b.Quantity); 
         public override Currency<T> operator *(Currency<T> a, Currency<T> b) => new Currency<T>(a.Quantity * b.Quantity);
         public override Currency<T> operator /(Currency<T> a, Currency<T> b) => new Currency<T>((int) Mathf.Round(a.Quantity / b.Quantity));
+    }
+
+    public class CurrencyData
+    {
+        public string name;
+        public string value;
     }
 }
