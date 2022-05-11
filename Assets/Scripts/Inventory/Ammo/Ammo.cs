bfaf766c51e1276ac498cb53127ff175fb9520c7
Matt Burns <pidiegames@gmail.com>
2022-05-11 18:23:00 -0400

    cleaned up Ammo and UpdateQuantity

--- Assets/Scripts/Inventory/Ammo/Ammo.cs
+++ Assets/Scripts/Inventory/Ammo/Ammo.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace Inventory.Ammo
 {
-	public abstract class Ammo : Item<Ammo>, IStackable
+	public abstract class Ammo : Item
 	{
 		public int Quantity { get; set; }
 		public int MaxStackSize { get; set; } = 40;
@@ -12,37 +12,28 @@ namespace Inventory.Ammo
 		public virtual bool UpdateQuantity(IStackable otherItem) => false;
 	}
 
-	public abstract class Ammo<T> : Ammo where T : Ammo<T>
+	public abstract class Ammo<T> : Ammo where T : IStackable
 	{
-		public override bool UpdateQuantity(IStackable otherItem)
-		{
-			if (otherItem.Quantity < 1)
-			{
-				Debug.LogWarning($"Cannot have a stack size of less than 1. {otherItem}: {otherItem.Quantity}");
-				return false;
-			}
-			
-			if (otherItem is T)
-			{
-				var otherQuantity = otherItem.Quantity;
-				if (Quantity + otherQuantity > MaxStackSize)
-				{
-					otherItem.Quantity = otherQuantity + Quantity - MaxStackSize;
-					Quantity = MaxStackSize;
-				}
-				else if (Quantity == MaxStackSize)
-				{
-					Debug.LogWarning("Already at max stack size!");
-					return false;
-				}
-				else
-				{
-					Quantity += otherQuantity;
-					otherItem.Quantity -= otherQuantity;
-				}
-				return true;
-			}
-			return false;
-		}
-	}
+		public override bool UpdateQuantity(Ammo<T> stack)
+        {
+            var sum = Quantity + stack.Quantity;
+
+            if (Quantity != MaxStackSize)
+            {
+                if (sum > MaxStackSize)
+                {
+                    stack.Quantity = sum - MaxStackSize;
+                    Quantity = MaxStackSize;
+                }
+                else
+                {
+                    Quantity += stack.Quantity;
+                    stack.Quantity -= stack.Quantity;
+                }
+                return true;
+            }
+
+            return false;
+        }
+    }
 }
