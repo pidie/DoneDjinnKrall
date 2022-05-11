e7ff9d2adf82c47829246d78b11db825c67fe400
Matt Burns <pidiegames@gmail.com>
2022-05-11 18:04:05 -0400

    added IEquippable

---
+++ Assets/Scripts/Inventory/IEquippable.cs
@@ -0,0 +1,22 @@
+/// <summary> Used by items that can be equipped in the inventory. </summary>
+public interface IEquippable
+{
+    public bool IsEquipped;
+
+    public void EquipItem(Item item);
+
+    // item can be added to a list of unequipped items
+    /* 
+    EquipItem(Item item)
+    {
+        if (!unequippedItems.Contains(item))
+            throw new Exception($"Item must be in inventory and unequipped; tried {item.name} ")
+            
+        if (activeItem != null)
+            unequippedItems.Add(UnequipItem(activeItem));
+
+        activeItem = item;
+    }
+    */
+    public Item UnequipItem(Item item);
+}
