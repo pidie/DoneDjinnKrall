58e431cded2bff85a5c9d2fb8b27b6dec76cb42b
Matt Burns <mmburns88@gmail.com>
2022-05-11 22:38:11 -0400

    fixed operator overloads

--- Assets/Scripts/Inventory/Currencies/Currency.cs
+++ Assets/Scripts/Inventory/Currencies/Currency.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Inventory.Currencies
 {
@@ -8,22 +9,48 @@ namespace Inventory.Currencies
 
 		public int Quantity => quantity;
 
-		public CurrencyData currencyData { get; private set; }
+		public CurrencyData currencyData { get; protected set; }
 
 		protected Currency() => quantity = 0;
-		public Currency(int value) => quantity = value;
+		protected Currency(int value) => quantity = value;
 
 		public void Bind(CurrencyData data) => currencyData = data;
 	}
 
 	public abstract class Currency<T> : Currency where T : IAccruable
 	{
+		public static Currency<T> operator +(Currency<T> a, Currency<T> b)
+		{
+			a.quantity = a.Quantity + b.Quantity;
+			return a;
+		}
+
+		public static Currency<T> operator -(Currency<T> a, Currency<T> b)
+		{
+			a.quantity = a.Quantity - b.Quantity;
+			return a;
+		}
+
+		public static Currency<T> operator *(Currency<T> a, Currency<T> b)
+		{
+			a.quantity = a.Quantity * b.Quantity;
+			return a;
+		}
+
+		/// <summary> THIS OPERATOR WILL AUTOMATICALLY ROUND TO THE NEAREST EVEN INTEGER </summary>
+		public static Currency<T> operator /(Currency<T> a, Currency<T> b)
+		{
+			a.quantity = (int) Mathf.Round(a.Quantity / b.Quantity);
+			return a;
+		}
+		
 		// public static Currency<T> operator +(Currency<T> a, Currency<T> b) => new Currency<T>(a.Quantity + b.Quantity);
 		// public static Currency<T> operator -(Currency<T> a, Currency<T> b) => new Currency<T>(a.Quantity - b.Quantity); 
 		// public static Currency<T> operator *(Currency<T> a, Currency<T> b) => new Currency<T>(a.Quantity * b.Quantity);
 		// public static Currency<T> operator /(Currency<T> a, Currency<T> b) => new Currency<T>((int) Mathf.Round(a.Quantity / b.Quantity));
 	}
 
+	[Serializable]
 	public class CurrencyData
 	{
 		public string name;
