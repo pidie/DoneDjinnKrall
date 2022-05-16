7f62fabc06e6a3871b5253fe80ffe3dc152551dc
Matt Burns <pidiegames@gmail.com>
2022-05-15 22:37:24 -0400

    reorganized Essence

--- Assets/Scripts/Inventory/Currencies/Essence.cs
+++ Assets/Scripts/Inventory/Currencies/Essence.cs
@@ -3,10 +3,15 @@
 namespace Inventory.Currencies
 {
 	[CreateAssetMenu(menuName = "Essence/Essence", fileName = "New Essence")]
-	public class Essence : IAccruable
+	public class Essence : Currency<EssenceType>
 	{
-		public EssenceType essenceType;
-		public string name => $"{essenceType} Essence";
+		public string name => $"{EssenceType} Essence";
 		public Sprite icon => essenceType.icon;
+
+		public Essence(EssenceType e)
+		{
+			name = e.name;
+			icon = e.icon;
+		}
 	}
 }
